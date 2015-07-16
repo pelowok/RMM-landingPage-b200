faeae718147f0b11a8b6543360cfc3c4361df804
Zak PeloJoaquin <zak@mynameiszak.com>
2015-07-16 12:43:03 -0400

    layout almost completed, all lorem content

--- app/scripts/hello.js
+++ app/scripts/hello.js
@@ -1,25 +1,8 @@
 
-'use strict';
+/* jshint white:false */
 
-//  file created 2015-07-15
-//    by mynameiszak.com
-//    <zak@mynameiszak.com>
+(function() {
+  'use strict';
+  console.log('\'Allo from CoffeeScript!');
 
-var MNIZ = MNIZ || {};
-
-MNIZ.Hello = (function($) {
-
-  var pub = {};
-
-  var helloFunction = function (b) {
-    MNIZ.Main.mainFunction(b);
-  };
-
-  pub.helloFunction = helloFunction;
-
-  return pub;
-
-})(jQuery.noConflict());
-
-
-document.onload(MNIZ.Hello.helloFunction('o hai'));
+}).call(this);
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -38,9 +38,59 @@
 //
 //})(jQuery.noConflict());
 
+function toggleVisibility(id) {
 
-
-function toggle_visibility(id) {
     var e = document.getElementById(id);
-    e.style.display = ((e.style.display!='none') ? 'none' : 'block');
-}
+
+    var s1a = document.getElementById('step1hideme1');
+    var s1b = document.getElementById('step1hideme2');
+    var s2 = document.getElementById('formblock');
+
+    console.log('e.id : ' + e.id);
+
+    switch (e)
+    {
+        case s1a:
+            console.log(s1a);
+            if(s1a.style.display === 'none'){
+                s1a.style.display = 'block';
+                s1b.style.display = 'none';
+                s2.style.display = 'block';
+            } else {
+                s1a.style.display = 'none';
+                s1b.style.display = 'none';
+                s2.style.display = 'none';
+            }
+            break;
+
+        case s1b:
+            console.log(s1b);
+            if(s1b.style.display === 'none'){
+                s1a.style.display = 'none';
+                s1b.style.display = 'block';
+                s2.style.display = 'block';
+            } else {
+                s1a.style.display = 'none';
+                s1b.style.display = 'none';
+                s2.style.display = 'none';
+            }
+            break;
+
+
+        default:
+            console.log("Unknown<br />");
+            s1a.style.display = 'none';
+            s1b.style.display = 'none';
+            s2.style.display = 'none';
+            break;
+    }
+};
+
+
+//e1.style.display = ((e1.style.display!='none') ? 'none' : 'block');
+
+// formblock
+// step1hideme1
+// step1hideme2
+
+
