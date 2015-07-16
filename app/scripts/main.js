44aec41b887d2b83afed94a6085b38e67d22c348
Zak PeloJoaquin <zak@mynameiszak.com>
2015-07-16 16:32:39 -0400

    no content ready for review

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -45,6 +45,7 @@ function toggleVisibility(id) {
     var s1a = document.getElementById('step1hideme1');
     var s1b = document.getElementById('step1hideme2');
     var s2 = document.getElementById('formblock');
+    var cta = document.getElementById('main-cta');
 
     console.log('e.id : ' + e.id);
 
@@ -52,37 +53,73 @@ function toggleVisibility(id) {
     {
         case s1a:
             console.log(s1a);
-            if(s1a.style.display === 'none'){
-                s1a.style.display = 'block';
-                s1b.style.display = 'none';
+            if(s1a.style.opacity < 0.9){
+                s1a.style.opacity = 1;
+                s1a.style.borderColor = 'white';
+                s1b.style.opacity = 0.25;
+                s1b.style.borderColor = 'transparent';
                 s2.style.display = 'block';
+                scrolltest('formblock');
             } else {
-                s1a.style.display = 'none';
-                s1b.style.display = 'none';
+                s1a.style.opacity = 0.5;
+                s1b.style.opacity = 0.5;
                 s2.style.display = 'none';
             }
             break;
 
         case s1b:
             console.log(s1b);
-            if(s1b.style.display === 'none'){
-                s1a.style.display = 'none';
-                s1b.style.display = 'block';
+            if(s1b.style.opacity < 0.9){
+                s1a.style.opacity = 0.25;
+                s1a.style.borderColor = 'transparent';
+                s1b.style.opacity = 1;
+                s1b.style.borderColor = 'white';
                 s2.style.display = 'block';
+                scrolltest('formblock');
+
             } else {
-                s1a.style.display = 'none';
-                s1b.style.display = 'none';
+                s1a.style.opacity = 0.5;
+                s1b.style.opacity = 0.5;
                 s2.style.display = 'none';
             }
             break;
 
+        case cta:
+            scrolltest('main-cta');
+            break;
 
         default:
-            console.log("Unknown<br />");
-            s1a.style.display = 'none';
-            s1b.style.display = 'none';
+            console.log('err');
+            s1a.style.opacity = 0.5;
+            s1a.style.borderColor = 'transparent';
+            s1b.style.opacity = 0.5;
+            s1b.style.borderColor = 'transparent';
             s2.style.display = 'none';
             break;
+    }
+
+    function scrollTo(to, duration) {
+        if (document.body.scrollTop == to) return;
+        var diff = to - document.body.scrollTop;
+        var scrollStep = Math.PI / (duration / 10);
+        var count = 0, currPos;
+        var start = document.body.scrollTop;
+        var scrollInterval = setInterval(function(){
+            if (document.body.scrollTop != to) {
+                count = count + 1;
+                currPos = start + diff * (0.5 - 0.5 * Math.cos(count * scrollStep));
+                document.body.scrollTop = currPos;
+            }
+            else { clearInterval(scrollInterval); }
+        },10);
+    }
+
+    function scrolltest(elID)
+    {
+        console.log('test');
+
+        var dest = document.getElementById(elID);
+        scrollTo(dest.offsetTop, 500);
     }
 };
 
